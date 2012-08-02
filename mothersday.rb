8a8f35e34e4e6a6748901aafad974a714deb0adf
Katrina Owen <katrina.owen@gmail.com>
2012-08-02 11:20:07 +0200

    Add mothersday example

---
+++ mothersday.rb
@@ -0,0 +1,26 @@
+require 'active_support/all'
+
+module CalendarService
+
+  # in Norway
+  def date_for_mothersday(today = Time.now.utc)
+    date_for_parentsday(2, today)
+  end
+
+  def date_for_parentsday(month, today)
+    # Day number 8 will guarantee the second Sunday of the month
+    close_date = Time.utc(today.year, month, 8)
+    possible_parentsday = next_sunday_after(close_date)
+    if possible_parentsday >= today.beginning_of_day
+      actual_parentsday = possible_parentsday
+    else
+      actual_parentsday = next_sunday_after(close_date + 1.year)
+    end
+    return actual_parentsday
+  end
+
+  def next_sunday_after(time)
+    return time + ((7 - time.wday) % 7) * 1.day
+  end
+
+end
