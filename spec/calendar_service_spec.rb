a2ad87fdd5c1a6a3dffca5365a811c22248223a9
Katrina Owen <katrina.owen@gmail.com>
2012-09-01 15:10:38 +0200

    Add specs for defaults in calendar service

--- spec/calendar_service_spec.rb
+++ spec/calendar_service_spec.rb
@@ -4,11 +4,18 @@ describe CalendarService do
 
   subject { stub.extend(CalendarService) }
 
+  let(:now) { Time.now.utc }
+
   describe "mothersday" do
     it { subject.date_for_mothersday(Time.utc(1986, 2, 1)).should eq(Time.utc(1986, 2, 9)) }
     it { subject.date_for_mothersday(Time.utc(2012, 2, 1)).should eq(Time.utc(2012, 2, 12)) }
     it { subject.date_for_mothersday(Time.utc(2012, 2, 12)).should eq(Time.utc(2012, 2, 12)) }
     it { subject.date_for_mothersday(Time.utc(2012, 2, 13)).should eq(Time.utc(2013, 2, 10)) }
+
+    it "defaults to today" do
+      expected = subject.date_for_mothersday(Time.utc(now.year, now.month, now.day))
+      subject.date_for_mothersday.should eq(expected)
+    end
   end
 
   describe "fathersday" do
@@ -16,5 +23,11 @@ describe CalendarService do
     it { subject.date_for_fathersday(Time.utc(2012, 11, 1)).should eq(Time.utc(2012, 11, 11)) }
     it { subject.date_for_fathersday(Time.utc(2012, 11, 11)).should eq(Time.utc(2012, 11, 11)) }
     it { subject.date_for_fathersday(Time.utc(2012, 11, 12)).should eq(Time.utc(2013, 11, 10)) }
+
+    it "defaults to today" do
+      expected = subject.date_for_fathersday(Time.utc(now.year, now.month, now.day))
+      subject.date_for_fathersday.should eq(expected)
+    end
+
   end
 end
