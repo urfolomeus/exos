a73aecf4289e0c21b50cc7aafc58463465e58171
Katrina Owen <katrina.owen@gmail.com>
2012-09-14 20:58:48 +0200

    Add more tests on tinyurl

--- tinyurl/lookup_spec.rb
+++ tinyurl/lookup_spec.rb
@@ -12,7 +12,30 @@ describe Tinyurl do
 
   it "caches" do
     Tinyurl.should_receive(:lookup).once.and_return {"http://short.url"}
-    Tinyurl.get("http://kytrinyx.com")
-    Tinyurl.get("http://kytrinyx.com")
+    Tinyurl.get("http://google.com")
+    Tinyurl.get("http://google.com")
   end
+
+  it "just returns the original url on errors" do
+    result = stub(:code => 409)
+    Net::HTTP.stub(:start => result)
+    Tinyurl.get("http://example.com").should eq('http://example.com')
+  end
+
+  specify "well, not all errors" do
+    result = stub(:code => 500)
+    Net::HTTP.stub(:start => result)
+    # Not sure what actually causes the API to return 500
+    ->{ Tinyurl.get("http://something.com") }.should raise_error Tinyurl::InvalidUrlException
+  end
+
+  it "doesn't worry about invalid urls" do
+    Tinyurl.get("invalidurl").should eq("invalidurl")
+  end
+
+  it "swallows timeouts" do
+    Net::HTTP.stub(:start).and_raise(Timeout::Error)
+    Tinyurl.get("http://timeout.com").should eq("http://timeout.com")
+  end
+
 end
