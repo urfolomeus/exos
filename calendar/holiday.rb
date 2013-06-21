b41b58f1326c893b01ce73b78bd565c088ac8f38
Alan Gardner <urfolomeus@gmail.com>
2013-06-21 16:53:44 +0100

    Encapsulate logic better: fathers day should describe fathers day

--- calendar/holiday.rb
+++ calendar/holiday.rb
@@ -38,11 +38,17 @@ class MothersdayCalculator
   end
 end
 
-class FathersdayCalculator < ParentsDayCalculator
+class FathersdayCalculator
   NOVEMBER = 11
 
-  def initialize
-    super(NOVEMBER)
+  def for_year(year)
+    second_sunday_of(NOVEMBER, year)
+  end
+
+  def second_sunday_of(month, year)
+    first_day_of_month = Date.new(year, month, 1)
+    first_sunday = first_day_of_month.sunday
+    first_sunday + 1.week
   end
 end
 
@@ -58,7 +64,9 @@ module Calendar
 
     def date_for_fathersday(date_to_check = Date.today)
       fdc = FathersdayCalculator.new
-      fdc.next_date(date_to_check)
+      this_fathersday = fdc.for_year(date_to_check.year)
+      return this_fathersday if date_to_check <= this_fathersday
+      fdc.for_year(date_to_check.year + 1)
     end
   end
 end
