e1e2f0655f2d8394dbd3daa0056dccee3e2e0fb4
Alan Gardner <urfolomeus@gmail.com>
2013-06-21 16:59:18 +0100

    Encapsulate logic better: inline fathersday

--- calendar/holiday.rb
+++ calendar/holiday.rb
@@ -1,26 +1,17 @@
 require 'active_support/all'
 
-class FathersdayCalculator
-  NOVEMBER = 11
-
-  def for_year(year)
-    second_sunday_of(NOVEMBER, year)
-  end
-
-  def second_sunday_of(month, year)
-    first_day_of_month = Date.new(year, month, 1)
-    first_sunday = first_day_of_month.sunday
-    first_sunday + 1.week
-  end
-end
-
 # in Norway
 module Calendar
   module Holiday
     FEBRUARY = 2
+    NOVEMBER = 11
 
     def mothersday_for_year(year)
       second_sunday_of(FEBRUARY, year)
+    end
+
+    def fathersday_for_year(year)
+      second_sunday_of(NOVEMBER, year)
     end
 
     def second_sunday_of(month, year)
@@ -36,10 +27,9 @@ module Calendar
     end
 
     def date_for_fathersday(date_to_check = Date.today)
-      fdc = FathersdayCalculator.new
-      this_fathersday = fdc.for_year(date_to_check.year)
+      this_fathersday = fathersday_for_year(date_to_check.year)
       return this_fathersday if date_to_check <= this_fathersday
-      fdc.for_year(date_to_check.year + 1)
+      fathersday_for_year(date_to_check.year + 1)
     end
   end
 end
