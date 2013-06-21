daa098f83acb72479b59692c1428d9baf51159fc
Alan Gardner <urfolomeus@gmail.com>
2013-06-21 17:06:50 +0100

    Encapsulate logic better: tidy up

--- calendar/holiday.rb
+++ calendar/holiday.rb
@@ -6,11 +6,29 @@ module Calendar
     FEBRUARY = 2
     NOVEMBER = 11
 
-    def mothersday_for_year(year)
+    def date_for_mothersday(date_to_check = Date.today)
+      if date_to_check <= mothersday_for(date_to_check.year)
+        mothersday_for(date_to_check.year)
+      else
+        mothersday_for(date_to_check.year + 1)
+      end
+    end
+
+    def date_for_fathersday(date_to_check = Date.today)
+      if date_to_check <= fathersday_for(date_to_check.year)
+        fathersday_for(date_to_check.year)
+      else
+        fathersday_for(date_to_check.year + 1)
+      end
+    end
+
+    private
+
+    def mothersday_for(year)
       second_sunday_of(FEBRUARY, year)
     end
 
-    def fathersday_for_year(year)
+    def fathersday_for(year)
       second_sunday_of(NOVEMBER, year)
     end
 
@@ -18,20 +36,6 @@ module Calendar
       first_day_of_month = Date.new(year, month, 1)
       first_sunday = first_day_of_month.sunday
       first_sunday + 1.week
-    end
-
-    def date_for_mothersday(date_to_check = Date.today)
-      if date_to_check <= mothersday_for_year(date_to_check.year)
-        mothersday_for_year(date_to_check.year)
-      else
-        mothersday_for_year(date_to_check.year + 1)
-      end
-    end
-
-    def date_for_fathersday(date_to_check = Date.today)
-      this_fathersday = fathersday_for_year(date_to_check.year)
-      return this_fathersday if date_to_check <= this_fathersday
-      fathersday_for_year(date_to_check.year + 1)
     end
   end
 end
