3751f331aa1a5b6ed062d68694a476006940674a
Alan Gardner <urfolomeus@gmail.com>
2013-06-21 12:24:41 +0100

    Passing a lot of state about so trying out a class-based approach

--- calendar/holiday.rb
+++ calendar/holiday.rb
@@ -1,5 +1,29 @@
 require 'active_support/all'
 
+class ParentsDayCalculator
+  def initialize(parentsday_month)
+    @parentsday_month = parentsday_month
+  end
+
+  def next_parentsday(date_to_check)
+    calc_parentsday(date_to_check.year, date_to_check)
+  end
+
+  private
+
+  def calc_parentsday(year, date_to_check)
+    parentsday = second_sunday_of(year, @parentsday_month)
+    return parentsday if date_to_check <= parentsday
+    calc_parentsday(year + 1, date_to_check)
+  end
+
+  def second_sunday_of(year, month)
+    first_day_of_month = Date.new(year, month, 1)
+    first_sunday = first_day_of_month.sunday
+    first_sunday + 1.week
+  end
+end
+
 # in Norway
 module Calendar
   module Holiday
@@ -18,19 +42,8 @@ module Calendar
     private
 
     def date_for_parentsday(month, date_to_check)
-      next_parentsday(date_to_check.year, month, date_to_check)
-    end
-
-    def next_parentsday(year, month, date_to_check)
-      parentsday = second_sunday_of(year, month)
-      return parentsday if date_to_check <= parentsday
-      next_parentsday(year + 1, month, date_to_check)
-    end
-
-    def second_sunday_of(year, month)
-      first_day_of_month = Date.new(year, month, 1)
-      first_sunday = first_day_of_month.sunday
-      first_sunday + 1.week
+      pdc = ParentsDayCalculator.new(month)
+      pdc.next_parentsday(date_to_check)
     end
   end
 end
