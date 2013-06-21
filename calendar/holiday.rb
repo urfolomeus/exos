fd594de1fd5b5af62fec99a64e3e75f3b7dd455d
Alan Gardner <urfolomeus@gmail.com>
2013-06-21 12:40:09 +0100

    Some tidy up

--- calendar/holiday.rb
+++ calendar/holiday.rb
@@ -5,16 +5,16 @@ class ParentsDayCalculator
     @parentsday_month = parentsday_month
   end
 
-  def next_parentsday(date_to_check)
-    calc_parentsday(date_to_check.year, date_to_check)
+  def next_date(date_to_check)
+    calc_next_date(date_to_check.year, date_to_check)
   end
 
   private
 
-  def calc_parentsday(year, date_to_check)
+  def calc_next_date(year, date_to_check)
     parentsday = second_sunday_of(year)
     return parentsday if date_to_check <= parentsday
-    calc_parentsday(year + 1, date_to_check)
+    calc_next_date(year + 1, date_to_check)
   end
 
   def second_sunday_of(year)
@@ -45,12 +45,12 @@ module Calendar
   module Holiday
     def date_for_mothersday(date_to_check = Date.today)
       mdc = MothersdayCalculator.new
-      mdc.next_parentsday(date_to_check)
+      mdc.next_date(date_to_check)
     end
 
     def date_for_fathersday(date_to_check = Date.today)
       fdc = FathersdayCalculator.new
-      fdc.next_parentsday(date_to_check)
+      fdc.next_date(date_to_check)
     end
   end
 end
